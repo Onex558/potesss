7bea9e08939c3aaebcefeec781b8de49ce552dde
Onex558 <thomasfigy@icloud.com>
2026-09-12 17:51:49 -0500

    Delete proxy html directory

--- proxy html/classwork.lol/kiwi-antiblock.js
+++
@@ -1,5 +0,0 @@
-/* Compatibility stub: legacy pages still reference this file, but the
-   ad-block detector and blocking screen have intentionally been removed. */
-(function () {
-  window.__kiwiAntiblockLoaded = true;
-})();
